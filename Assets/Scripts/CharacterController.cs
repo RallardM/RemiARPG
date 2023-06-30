5136c880b61780a1bd4654c4378c2cadd3b082fe
Remi Allard Mayer <remallmay@outlook.com>
2023-06-30 15:00:41 -0400

    Animation and asset first draft

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    private Animator m_animator;
+    private SpriteRenderer m_spriteRenderer;
+
+    private void Awake()
+    {
+        m_animator = GetComponent<Animator>();
+        m_spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_animator.SetBool("CanWalk", true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            m_spriteRenderer.flipX = true;
+            m_animator.SetFloat("Speed", 1.0f);
+            return;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            m_spriteRenderer.flipX = false;
+            m_animator.SetFloat("Speed", 1.0f);
+            return;
+        }
+
+        m_animator.SetFloat("Speed", 0.0f);
+    }
+}
